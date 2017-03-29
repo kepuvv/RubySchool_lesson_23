1a2f9e0e1a23f706fcd92a4baec53ca24010a429
Kepuvv <renard.ru@gmail.com>
2017-03-29 16:40:53 +0300

    Getting params from post form

--- app.rb
+++ app.rb
@@ -13,3 +13,12 @@ end
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+	@username = params[:username]
+	@phone = params[:phone]
+	@date = params[:datetime]
+	@barber_id = params[:barber_id]
+	
+	erb "OK!, username is #{@username}, #{@phone}, #{@date}, #{@barber_id}"
+end
